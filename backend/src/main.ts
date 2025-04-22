f4f7e0fd0a235bfe2787fe5a41bd1fb65fdcb349
jragunanthan <ragunanthan.j@ib-systems.org>
2025-04-22 17:11:30 +0530

    Implement swagger for hello world api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3021);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fusion HBAR Service API')
+    .setDescription('TBD')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',},
+      'access-token',)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  const port = process.env.PORT ?? 3021;
+  await app.listen(port);
 }
 bootstrap();
