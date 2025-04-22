206fb1214fb6eec2a2c25b2b7649611c322ec6bb
Abhijith H R <abhijith.hr@ib-systems.org>
2025-04-22 17:24:57 +0200

    Endpoint structure and Hedera SDK client provider

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DidService } from './endpoints/did/did.service';
+import { DidController } from './endpoints/did/did.controller';
+import { VcController } from './endpoints/vc/vc.controller';
+import { AccountController } from './endpoints/account/account.controller';
+import { VcService } from './endpoints/vc/vc.service';
+import { AccountService } from './endpoints/account/account.service';
+import { HederaClientProvider } from './hashgraph.provider';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, DidController, VcController, AccountController],
+  providers: [AppService, DidService, VcService, AccountService, HederaClientProvider],
+  exports: [HederaClientProvider]
 })
 export class AppModule {}
